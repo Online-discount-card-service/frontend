eaedeccd71be1cd55e79e1e78bfcce9518b37295
kavabunga <kavabunga@gmail.com>
2023-11-16 12:41:28 +0200

    fix: fix build process

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,25 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'node:path';
 // https://vitejs.dev/config/
+
+function differMuiSourcemapsPlugins() {
+  const muiPackages = ['@mui/material', '@emotion/styled', '@emotion/react'];
+
+  return {
+    name: 'differ-mui-sourcemap',
+    transform(code: string, id: string) {
+      if (muiPackages.some((pkg) => id.includes(pkg))) {
+        return {
+          code: code,
+          map: null,
+        };
+      }
+    },
+  };
+}
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), differMuiSourcemapsPlugins()],
   resolve: {
     alias: {
       '~': path.resolve(__dirname, './src/'),
