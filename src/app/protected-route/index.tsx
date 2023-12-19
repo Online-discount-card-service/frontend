211fdada72c9765d06a3d73d0e4d2da49cbcf1be
Darya Kolotilina <colotilina.d@yandex.ru>
2023-12-19 23:15:49 +0200

    'fix: routing on page refresh'

--- src/app/protected-route/index.tsx
+++ src/app/protected-route/index.tsx
@@ -1,8 +1,18 @@
-import { Navigate, Outlet } from 'react-router-dom';
+import React from 'react';
+import { Await, Navigate, Outlet } from 'react-router-dom';
 import { useUser } from '~/shared/store/useUser';
 
 export const ProtectedRoute = () => {
   const user = useUser((state) => state.user);
-
-  return user ? <Outlet /> : <Navigate to="/" replace />;
+  // return !user ? <Navigate to="/" replace /> : <Outlet />;
+  return (
+    <React.Suspense>
+      <Await
+        // and is the promise we pass to Await
+        resolve={user}
+      >
+        {!user ? <Navigate to="/" replace /> : <Outlet />}
+      </Await>
+    </React.Suspense>
+  );
 };
